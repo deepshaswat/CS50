27c49166d624aaaae68c1964b7beeb71737aa042
Shaswat Deep <deepshaswat@gmail.com>
2016-07-14 23:24:51 +0530

    Commented caesar cipher

--- week2/pset2/caesar.c
+++ week2/pset2/caesar.c
@@ -5,34 +5,38 @@
 
 int main(int argc, char **argv){
 
-    if(argc < 2){
-        printf("Please enter an integer\n");
+    # to check exactly one argument is passed.
+    if(argc != 2){
+        printf("Please enter an integer and one argument\n");
         return 1;
     }
     string s = GetString();
+    #to convert the string to an integer passed as the argument.
     int k = atoi(argv[1]);
-    
+
+    #to encryption can only be done for the value of k from 0 -26.
     if(k > 26)
         k = k % 26;
-    
+
+    #encrypting and printing directly in the console
     for(int i = 0, n = strlen(s); i < n; i++){
         if(isalpha(s[i]) && isupper(s[i])){
             int c = ((int)s[i] + k);
             if(c > 90)
                 c -= 26;
             printf("%c", (char)c);
-        } 
+        }
         else if(isalpha(s[i]) && islower(s[i])){
             int c = ((int)s[i] + k);
             if(c > 122)
                 c -= 26;
             printf("%c", (char)c);
-        } 
+        }
         else{
             printf("%c", s[i]);
         }
     }
     printf("\n");
-    
-    return 0;   
+
+    return 0;
 }
